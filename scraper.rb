c2968bc03b4457296900e66bf536a540e3a6fd9e
Tony Bowden <tony@tmtm.com>
2017-01-13 13:25:38 +0000

    rubocop -a

--- scraper.rb
+++ scraper.rb
@@ -1,5 +1,6 @@
 #!/bin/env ruby
 # encoding: utf-8
+# frozen_string_literal: true
 
 require 'scraperwiki'
 require 'nokogiri'
@@ -11,17 +12,17 @@ OpenURI::Cache.cache_path = '.cache'
 
 class String
   def tidy
-    self.gsub(/[[:space:]]+/, ' ').strip
+    gsub(/[[:space:]]+/, ' ').strip
   end
 end
 
 def noko_for(url)
-  Nokogiri::HTML(open(url).read) 
+  Nokogiri::HTML(open(url).read)
 end
 
 def dob_from(str)
   return if str.to_s.empty?
-  Date.parse(str.sub('Born on ','')).to_s
+  Date.parse(str.sub('Born on ', '')).to_s
 end
 
 def scrape_list(url)
@@ -41,21 +42,21 @@ def scrape_person(sortname, url)
   party_id, party = party_data.match(/(.*?) - (.*?) \(.*?\)/).captures rescue binding.pry
   party.sub!(/\s*Deputy Group\s*/, '')
 
-  data = { 
-    id: url.to_s[/(\d+)$/, 1],
-    name: info_box.css('h3').text.strip,
-    sort_name: sortname,
-    birth_date: dob_from(info_box.xpath('.//span[contains(.,"Born on")]').text),
+  data = {
+    id:           url.to_s[/(\d+)$/, 1],
+    name:         info_box.css('h3').text.strip,
+    sort_name:    sortname,
+    birth_date:   dob_from(info_box.xpath('.//span[contains(.,"Born on")]').text),
     contact_form: info_box.css('a.outputLinkEx[href*="dz-rs"]/@href').text,
-    constituency: panel_box.xpath('.//span[contains(.,"Electoral district")]/text()').text.sub(': ','').strip,
-    party: party,
-    party_id: party_id,
-    image: info_box.css('img.graphicImageEx/@src').text,
-    term: '7',
-    source: url.to_s,
+    constituency: panel_box.xpath('.//span[contains(.,"Electoral district")]/text()').text.sub(': ', '').strip,
+    party:        party,
+    party_id:     party_id,
+    image:        info_box.css('img.graphicImageEx/@src').text,
+    term:         '7',
+    source:       url.to_s,
   }
   data[:image] = URI.join(url, data[:image]).to_s unless data[:image].to_s.empty?
-  ScraperWiki.save_sqlite([:id, :term], data)
+  ScraperWiki.save_sqlite(%i(id term), data)
 end
 
 scrape_list('http://www.dz-rs.si/wps/portal/en/Home/ODrzavnemZboru/KdoJeKdo/PoslankeInPoslanci/PoAbecedi')
