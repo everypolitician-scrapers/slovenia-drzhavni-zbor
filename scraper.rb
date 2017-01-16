1ce3449d412c5b680dcc4d333f20ac4157456b94
Tony Bowden <tony@tmtm.com>
2017-01-16 16:31:39 +0000

    Rewrite using Scraped

--- scraper.rb
+++ scraper.rb
@@ -11,48 +11,91 @@ require 'scraperwiki'
 # OpenURI::Cache.cache_path = '.cache'
 require 'scraped_page_archive/open-uri'
 
-def noko_for(url)
-  Nokogiri::HTML(open(url).read)
-end
+class MembersPage < Scraped::HTML
+  decorator Scraped::Response::Decorator::AbsoluteUrls
 
-def dob_from(str)
-  return if str.to_s.empty?
-  Date.parse(str.sub('Born on ', '')).to_s
-end
-
-def scrape_list(url)
-  noko = noko_for(url)
-  noko.css('ul.podnaslovUL p.podnaslovOsebaLI a').each do |a|
-    scrape_person(a.text.tidy, URI.join(url, a.attr('href')))
+  field :members do
+    noko.css('ul.podnaslovUL p.podnaslovOsebaLI a').map do |a|
+      {
+        sort_name: a.text.tidy,
+        source:    a.attr('href'),
+      }
+    end
   end
 end
 
-def scrape_person(sortname, url)
-  noko = noko_for(url)
+class MemberPage < Scraped::HTML
+  decorator Scraped::Response::Decorator::AbsoluteUrls
 
-  info_box = noko.css('table.panelGrid')
-  panel_box = noko.css('.panelBox100')
+  field :id do
+    url.to_s[/(\d+)$/, 1]
+  end
 
-  party_data = panel_box.css('a[href*="PoslanskaSkupina"]').text
-  party_id, party = party_data.match(/(.*?) - (.*?) \(.*?\)/).captures rescue binding.pry
-  party.sub!(/\s*Deputy Group\s*/, '')
+  field :name do
+    info_box.css('h3').text.strip
+  end
 
-  data = {
-    id:           url.to_s[/(\d+)$/, 1],
-    name:         info_box.css('h3').text.strip,
-    sort_name:    sortname,
-    birth_date:   dob_from(info_box.xpath('.//span[contains(.,"Born on")]').text),
-    contact_form: info_box.css('a.outputLinkEx[href*="dz-rs"]/@href').text,
-    constituency: panel_box.xpath('.//span[contains(.,"Electoral district")]/text()').text.sub(': ', '').strip,
-    party:        party,
-    party_id:     party_id,
-    image:        info_box.css('img.graphicImageEx/@src').text,
-    term:         '7',
-    source:       url.to_s,
-  }
-  data[:image] = URI.join(url, data[:image]).to_s unless data[:image].to_s.empty?
-  ScraperWiki.save_sqlite(%i(id term), data)
+  field :birth_date do
+    dob_from(info_box.xpath('.//span[contains(.,"Born on")]').text)
+  end
+
+  field :contact_form do
+    info_box.css('a.outputLinkEx[href*="dz-rs"]/@href').text
+  end
+
+  field :constituency do
+    panel_box.xpath('.//span[contains(.,"Electoral district")]/text()').text.sub(': ', '').strip
+  end
+
+  field :party do
+    party_data.last
+  end
+
+  field :party_id do
+    party_data.first
+  end
+
+  field :image do
+    info_box.css('img.graphicImageEx/@src').text
+  end
+
+  field :term do
+    '7'
+  end
+
+  private
+
+  def dob_from(str)
+    return if str.to_s.empty?
+    Date.parse(str.sub('Born on ', '')).to_s
+  end
+
+  def info_box
+    noko.css('table.panelGrid')
+  end
+
+  def panel_box
+    noko.css('.panelBox100')
+  end
+
+  def party_data
+    party_data = panel_box.css('a[href*="PoslanskaSkupina"]').text
+    party_id, party = party_data.match(/(.*?) - (.*?) \(.*?\)/).captures rescue binding.pry
+    party.sub!(/\s*Deputy Group\s*/, '')
+    [party_id, party]
+  end
 end
 
+def scrape(h)
+  url, klass = h.to_a.first
+  klass.new(response: Scraped::Request.new(url: url).response)
+end
+
+start = 'http://www.dz-rs.si/wps/portal/en/Home/ODrzavnemZboru/KdoJeKdo/PoslankeInPoslanci/PoAbecedi'
+data = scrape(start => MembersPage).members.map do |mem|
+  mem.merge(scrape(mem[:source] => MemberPage).to_h)
+end
+# puts data.map { |r| r.sort_by { |k, _| k }.to_h }
+
 ScraperWiki.sqliteexecute('DELETE FROM data') rescue nil
-scrape_list('http://www.dz-rs.si/wps/portal/en/Home/ODrzavnemZboru/KdoJeKdo/PoslankeInPoslanci/PoAbecedi')
+ScraperWiki.save_sqlite(%i(id term), data)
